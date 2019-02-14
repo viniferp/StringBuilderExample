51b1eef6a2f63d32e1cbd8dc7af5a5529a1e6bb7
Vinicius Ferreira <augustfp18@gmail.com>
2019-02-13 23:34:39 -0200

    Adição classes Program, Comment e Post

---
+++ src/Entities/Comment.java
@@ -0,0 +1,20 @@
+package Entities;
+
+public class Comment {
+	
+	private String text;
+
+	public Comment(String text) {
+		super();
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+}
---
+++ src/Entities/Post.java
@@ -0,0 +1,59 @@
+package Entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+	private Date moment;
+	private String title;
+	private String content;
+	private int likes;
+	private List<Comment> comments;
+	
+	public Post(Date moment, String title, String content, int likes) {
+		super();
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+		comments = new ArrayList<Comment>();
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public void setLikes(int likes) {
+		this.likes = likes;
+	}
+	
+	public void addComment(Comment c) {
+		comments.add(c);
+	}
+}
